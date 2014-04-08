373cb8ece40ac627fa2cfded43f5be0660afe3d0
Apolikamixitos <ayoubuto@gmail.com>
2014-04-08 13:18:36 +0100

    Adding Arduino commands to communicate with the arduino directly and changing the way 'bridge.py' handle the data

--- bridge.py
+++ bridge.py
@@ -25,25 +25,40 @@ class Bridge:
             plugin = __import__(plugin_name[1]+'.plugin', fromlist=[''])
             configuration = __import__(plugin_name[1]+'.configuration', fromlist=[''])
             name = plugin_name[0]
+            arduino_cmd = plugin_name[2]
             try:
                 print 'Initializing '+name+'\'s plugin thread ...'
-                thread.start_new_thread( self.Watch, (plugin,name,configuration.SLEEP_TIME,) )
+                thread.start_new_thread( self.Watch, (plugin,name,configuration.SLEEP_TIME,arduino_cmd,) )
             except:
                 print "Error: unable to start thread"
 
 
-    def Watch(self,plugin,name,sleeptime):
+    def Watch(self,plugin,name,sleeptime,arduino_cmd):
         while 1:
             print name+' plugin pins checking ...'
-            PINS = plugin.WS.GETPINS()
-            for pin in PINS:
-                self.serial_ins.write("FREQ:"+str(PINS[pin])+"\n")
-                #ser = serial.Serial(config.DEVICE, config.RATE)
-                #self.serial_ins.write("FREQ:3\n")
-                print pin,PINS[pin]
+            ws = plugin.WS.update_values()
+            #print plugin.__name__
+            for pin in config.ENABLED_PINS:
+                if config.ENABLED_PINS[pin] is not None:
+                    #Check if the current PIN is related with the plugin
+                    info = config.ENABLED_PINS[pin].split('.')
+
+                    #Get the first part ex: 'Facebook.inbox' => first part : 'Facebook'
+                    part_name = info[0]
+                    method = info[1]
+
+                    #Call the method from the instance
+                    if part_name == name:
+                        value = getattr(ws, method)()
+                        pin_number = pin.replace('PIN_','')
+                        print 'Pin '+str(pin_number)+' Val: '+str(value)
+
+                        #Send the value and the command to the pin
+                        self.serial_ins.write(arduino_cmd+" "+str(pin_number)+" "+str(value)+"\n")
+                        #print self.serial_ins.readline()
+
             print name+' plugin updated.'
             time.sleep(sleeptime)
-
 
 if __name__ == "__main__":
     bridge = Bridge()
--- configuration.py
+++ configuration.py
@@ -6,13 +6,26 @@ import wsplugins.facebook.configuration as FB
 DEVICE='COM7' #Windows
 RATE = 9600
 
+#Tuples where we define the name, the module path and the arduino command
 ENABLED_PLUGINS={
-    ('Facebook','wsplugins.facebook'),
+    ('Facebook','wsplugins.facebook','FB_CMD'),
     #('Twitter','wsplugins.twitter'),
 }
 
-
+#ArduinoUNO Model
 ENABLED_PINS = {
-    FB.PIN_FRIENDREQUESTS, #PIN 12
-    FB.PIN_NOTIFICATIONS, #PIN 10
+    'PIN_1':'Facebook.inbox',
+    'PIN_2':'Facebook.notifications',
+    #'PIN_3':'Twitter.notifications',
+    'PIN_4':None,
+    'PIN_5':None,
+    'PIN_6':None,
+    'PIN_7':None,
+    'PIN_8':None,
+    'PIN_9':None,
+    'PIN_10':None,
+    'PIN_11':None,
+    'PIN_12':None,
+    'PIN_13':None,
+    'PIN_14':None,
 }
